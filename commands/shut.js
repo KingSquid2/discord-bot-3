4d49270f38fac4188e1f9a613ab6915524aef457
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-08-13 17:50:45 -0500

    Create shut.js

---
+++ commands/shut.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'shut',
+    description: "this is a shut command!",
+    execute(message, args){
+        message.channel.send('https://cdn.discordapp.com/attachments/707448978404409394/732118606061633606/video0.mp4');
+    }
+}
