740ace0b00d89257a430fef6fac67d91c67a52b2
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-08-14 08:17:53 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 
 module.exports = {
     name: 'help',
+    +	cooldown: 5,
     description: "this is a help command!",
     execute(message, args){
         message.channel.send('here are the commands: ;help, ;hi, ;hello, ;ping, ;pong,');
